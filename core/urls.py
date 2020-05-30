45f5956a586c0eb174ad99bf6ee254ae92734e6e
beingbiplov <beingbiplov@gmail.com>
2020-05-30 13:42:45 +0545

    employee info update view

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('add-education-info', EEICreateView.as_view(), name='add-education-info'),    
     path('add-experience-info', EExICreateView.as_view(), name='add-experience-info'),    
     path('profile', profile, name='profile'),
+    path('update-personal-info/<int:pk>', EPIUpdateView.as_view(), name='update-personal-info'),
 
     path('register', register_views.register, name='register'),
 ]
