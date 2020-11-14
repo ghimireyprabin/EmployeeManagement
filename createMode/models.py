3e09e3efa40785886498814a81dd750da55792d8
beingbiplov <beingbiplov@gmail.com>
2020-11-14 11:24:21 +0545

    add task model

--- createMode/models.py
+++ createMode/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator , MinValueValidator
+from core.models import Department
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+	title = models.CharField(max_length=240)
+	description = models.TextField()
+	total_points = models.PositiveIntegerField(validators=[MinValueValidator(10), MaxValueValidator(100)])
+	department = models.ForeignKey(Department, on_delete=models.CASCADE)
+	deadline = models.DateTimeField()
+	assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='assigned_to_user')
+	created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='created_by_user')
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'{self.department.name}-{self.title}'
