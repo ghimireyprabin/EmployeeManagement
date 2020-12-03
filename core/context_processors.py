135e31536d7e718c876c8506898a6fbdd004a8ca
beingbiplov <beingbiplov@gmail.com>
2020-12-03 14:06:34 +0545

    add sidebar-navbar

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,4 @@
-from .models import EmployeeJobInfo
+from .models import EmployeeJobInfo, EmployeePersonalInfo
 
 def check_authorization(request):
 	if request.user.is_superuser:
@@ -7,17 +7,24 @@ def check_authorization(request):
 		user_is_admin = False
 	if request.user.is_authenticated:
 		user_is_authenticated = True
+		personalinfo = EmployeePersonalInfo.objects.filter(user = request.user)[0]
 		jobinfo = EmployeeJobInfo.objects.filter(user = request.user)[0]
 		if jobinfo and jobinfo.isManager == True:
 			user_is_manager = True
 		else:
 			user_is_manager = False
+		if personalinfo:
+			user_fullname = personalinfo.fullname
+		else:
+			user_fullname = request.user.username
 	else:
 		user_is_authenticated = False
 		user_is_manager = False
+		user_fullname = None
 
 	return {
 		'user_is_authenticated' : user_is_authenticated,
 		'user_is_admin': user_is_admin,
 		'user_is_manager' : user_is_manager,
+		'user_fullname' : user_fullname,
 	}
